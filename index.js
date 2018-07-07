ecaa885dd976881e7669db64355b6116dfa9768e
sanky1984@gmail.com <sanky1984@gmail.com>
2018-07-07 23:51:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+<<<<<<< HEAD
 
 var animal = 'dog'
 
 function myAnimal() {
+=======
+var animal = 'dog'
+
+function myAnimal() {
+
+  return animal
+}
+
+
+
+function yourAnimal() {
+var animal = 'cat';  
+>>>>>>> 34b8e1a607cbffeeed90396bfe6cf2d80795db29
   return animal
 }
 
 function yourAnimal() {
+<<<<<<< HEAD
   var animal = 'cat'
+=======
+>>>>>>> 34b8e1a607cbffeeed90396bfe6cf2d80795db29
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
 
+<<<<<<< HEAD
 var two = 2;
 function add2(n) {
 
@@ -21,10 +39,31 @@ function add2(n) {
 }
 
 function funkyFunction() {
+=======
+function add2(n) {
+  const two = 2
+  return n + two
+
+  // Feel free to move things around!
+  
+}
+
+  var theFunk = funkyFunction
+
+
+var funkyFunction = function() {
+>>>>>>> 34b8e1a607cbffeeed90396bfe6cf2d80795db29
   return function() {
     return "FUNKY!"
   }
 }
 
+<<<<<<< HEAD
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
 var theFunk = funkyFunction()();
+=======
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
+
+
+>>>>>>> 34b8e1a607cbffeeed90396bfe6cf2d80795db29
